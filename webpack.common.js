76de153ba5a06dba863a5d8e9c8b7446b51cac4d
OlyaLarchenko <55406108+OlyaLarchenko@users.noreply.github.com>
2022-01-27 13:44:55 +0200

    Change structure

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,51 @@
 const path = require('path');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
   },
   module: {
     rules: [
       {
         test: /\.(sass|scss)$/,
         use: [
-          "style-loader",
+          MiniCssExtractPlugin.loader,
           "css-loader",
           "sass-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: ["postcss-preset-env"],
+              },
+            },
+          },
         ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
     new CleanWebpackPlugin(),
-    new CopyPlugin({
+    new CopyWebpackPlugin({
       patterns: [
         { from: './src/images', to: 'images' },
       ]
